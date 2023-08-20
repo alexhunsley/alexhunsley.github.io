65b8839a829514478d0f86195cfbbf55568f7adc
Alex Hunsley <alex.hunsley@gmail.com>
2023-08-20 11:21:21 +0100

    If an SE code not found, show nicer message prompting user to get me to run the script again if the code is valid

--- se-generate-redirect-pages.py
+++ se-generate-redirect-pages.py
@@ -41,7 +41,8 @@ with open(filename, 'w') as file:
                     window.location.href = redirectMap[seCode];
                 }
                 else {
-                    document.getElementById("error_message").innerHTML = "Invalid or missing Swift Evolution proposal code: &quot;" + seCode + "&quot;";
+                    document.getElementById("error_message").innerHTML = "<p>Invalid or missing Swift Evolution proposal code: &quot;" + seCode + "&quot;</p>"
+                        + "<p>If that proposal code exists, ask <a href=&quot;https://github.com/alexhunsley&quot;>alex hunsley</a> to run <code>se-generate-redirect-pages.py</code> again.</p>";
                 } 
             }
             else {
